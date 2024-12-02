74908d5aa31e8b5512956f5233e0392508653efd
Achinit99 <achinit720@gmail.com>
2024-12-02 19:09:07 +0530

    update index.php

--- index.php
+++ index.php
@@ -10,5 +10,44 @@
       <a href="./index.php" class="navbar-brand">Travel LK</a>
       <a href="./login.php" class="btn btn-success btn-sm">Login</a>
     </nav>
+
+    <main class="my-5">
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Welcome to Central Province</h1>
+          <p class="text-muted lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Suscipit eligendi aliquid, id voluptate consequatur magni unde blanditiis corporis quaerat cumque! 
+            Quasi velit, neque quod beatae asperiores consectetur numquam vero perspiciatis!</p>
+        </div>
+      </section>
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+            <div class="row">
+              <div class="col-md-4">
+                  <div class="card mb-4 box-shadow">
+                     <img class="card-img-top" style="height:200px;" src="./img/hotel1.jpg" alt="Hotel img">
+
+                     <div class="card-body">
+                        <p class="card-text">
+                          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi atque laboriosam aperiam, 
+                          itaque blanditiis molestias labore commodi dolor saepe. Rerum odio illum mollitia molestiae 
+                          ad tempora similique vitae magni deleniti.
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                          <div class="btn-group">
+                              <button type="button" class="btn btn-outline-primary btn-sm">View</button>
+                          </div>
+                        </div>
+                     </div>
+
+                  </div>
+              </div>
+            </div>
+          
+        </div>
+      </div>
+    </main>
   </body>
 </html>
