5fb537b7f57c690cdca639f2adf477158c9b1945
Achinit99 <achinit720@gmail.com>
2024-12-03 22:45:28 +0530

    Update Admin Dashboard

--- admin/adashboard.php
+++ admin/adashboard.php
@@ -1,1 +1,45 @@
-<h1>Admin Dashboard</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Travel LK</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light justyfy-content-between mx-3">
+      <a href="../index.php" class="navbar-brand">Travel LK</a>
+      <a href="./alogout.php" class="btn btn-warning btn-sm">Admin Logout</a>
+    </nav>
+
+    <main class="my-5">
+      <div class="row w-75 mx-auto">
+        <div class="container">
+          <h3 class="text-center mx-2">Welcome to Admin Dashboard</h3>
+        </div>
+
+        <div class="container text-start">
+          <a href="#" class="btn btn-primary btn-sm mx-2">Add New User</a>
+        </div>
+
+        <table class="table mt-2">
+          <thead class="table-start">
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>City</th>
+            <th>Action</th>
+          </thead>
+        </table>
+
+      </div>
+    </main>
+
+    <!-- footer section -->
+     <footer class="fixed-bottom text-muted">
+      <p class="text-center">2024 &copy; Achini Thathsarani</p>
+     </footer>
+
+
+  </body>
+</html>
